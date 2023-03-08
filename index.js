04fc08e300bd8cc2cf25fe2f933d9b7daf885805
terry-eric <67619529+terry-eric@users.noreply.github.com>
2023-03-08 15:28:17 +0800

    test2

--- index.js
+++ index.js
@@ -73,10 +73,10 @@ function handleNotifications(event) {
     a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
   }
   // document.querySelector("#log").value += a.join(' ')
-  let d = battery_func(value)
+  let d = battery_func(a)
   log(JSON.stringify(d));
   log(a);
-  log(a.join(','));
+  // log(a.join(','));
 
 }
 
