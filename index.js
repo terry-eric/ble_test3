03d64e9523863a7b05b43daa4dcc8a30d10216eb
terry-eric <67619529+terry-eric@users.noreply.github.com>
2023-04-28 09:44:39 +0800

    test1

--- index.js
+++ index.js
@@ -151,8 +151,8 @@ function callback(event) {
 
 function bytes2int16(bytes) {
   var view = new DataView(new ArrayBuffer(2));
-  view.setUint8(0, bytes[0]);
-  view.setUint8(1, bytes[1]);
+  view.setUint8(0, bytes[1]);
+  view.setUint8(1, bytes[0]);
   return view.getInt16(0, true); // true indicates little-endian byte order
 }
 // function bytes2int16(high, low) {
